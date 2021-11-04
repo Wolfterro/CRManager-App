7d76416cba5686b06c65db9e757d094242bd2717
Wolfgang <wolfgang.almeida@inchurch.com.br>
2021-11-03 23:53:12 -0300

    Removing unused import

--- app/src/main/java/com/github/wolfterro/crmanager/login/UserProfile.java
+++ app/src/main/java/com/github/wolfterro/crmanager/login/UserProfile.java
@@ -1,7 +1,6 @@
 package com.github.wolfterro.crmanager.login;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
