589210d8adcca11bd11a6feb1da505dba4837591
Wolfgang <wolfgang.almeida@inchurch.com.br>
2022-05-30 15:55:15 -0300

    Debugging URL

--- app/src/main/java/com/github/wolfterro/crmanager/utils/Utils.java
+++ app/src/main/java/com/github/wolfterro/crmanager/utils/Utils.java
@@ -131,7 +131,7 @@ public class Utils {
 
     private static String getBaseURL() {
         if(isDebug) {
-            return "https://backend-crmanager.herokuapp.com/"; // "http://192.168.0.219:8000/";
+            return "http://192.168.0.219:8000/";
         } else {
             return "";
         }
